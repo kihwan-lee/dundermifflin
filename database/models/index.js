9b3fc82176cec406bd017c33c339edbd78e8c0f6
kihwan-lee <kihwanlee97@gmail.com>
2020-12-11 23:15:38 -0600

    Adding seed data

--- database/models/index.js
+++ database/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectionString = 
-  process.env.MONGODB_URI || "mongodb://localhost:27017/DunderMifflin";
+  process.env.MONGODB_URI || "mongodb://localhost:27017/dundermifflin";
 
 const configOptions = {
   useNewUrlParser: true,
